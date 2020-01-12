0ebd6083efdfd75441afa41cbf7220c8fd55cc62
Valentin Bizgan <valentin.bizzz@gmail.com>
2020-01-12 11:29:46 +0000

    Updates

---
+++ precache-manifest.f24ed4274c3778c75dbdd0d2082f95e3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c48a7dd5df813f11e12aaadfdd09b81",
+    "url": "/Robofren/index.html"
+  },
+  {
+    "revision": "c8890da5dfac99701af1",
+    "url": "/Robofren/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d56b7a727bac97fa22a7",
+    "url": "/Robofren/static/css/main.f9a5d040.chunk.css"
+  },
+  {
+    "revision": "c8890da5dfac99701af1",
+    "url": "/Robofren/static/js/2.a98415d0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Robofren/static/js/2.a98415d0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d56b7a727bac97fa22a7",
+    "url": "/Robofren/static/js/main.016e6b8e.chunk.js"
+  },
+  {
+    "revision": "4e3a7d4be56ab0138c9a",
+    "url": "/Robofren/static/js/runtime-main.5762f36b.js"
+  },
+  {
+    "revision": "a5e91aa00ccefc3ed9b852f75572414d",
+    "url": "/Robofren/static/media/fifawelcome1.3.a5e91aa0.woff"
+  }
+]);
